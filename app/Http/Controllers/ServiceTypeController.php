5e6f6b41a729dbf2b9eeee39a2b6ed33aa3a13fe
hamid <hamidkarim2002@gmail.com>
2026-06-15 15:41:05 +0800

    feat: CHG-004 ServiceTypeController index passes feeGroups + modes

--- app/Http/Controllers/ServiceTypeController.php
+++ app/Http/Controllers/ServiceTypeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreServiceFeeRequest;
+use App\Models\ServiceFee;
 use App\Models\ServiceType;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -13,8 +15,12 @@ class ServiceTypeController extends Controller
 {
     public function index(): Response
     {
+        $fees = ServiceFee::orderBy('service_type')->orderBy('option')->get();
+
         return Inertia::render('ServiceTypes/Index', [
             'serviceTypes' => ServiceType::orderBy('name')->get(['id', 'name', 'requires_next_service']),
+            'feeGroups'    => $fees->groupBy('service_type'),
+            'modes'        => StoreServiceFeeRequest::MODES,
         ]);
     }
 
